2fea3a20d6cc581149e95b8818be6fb2ccaacd73
bow <bow@bow.web.id>
2012-01-19 15:11:00 +0100

    server.py now serves "site" by default

--- volt/server.py
+++ volt/server.py
@@ -3,13 +3,15 @@
 """HTTP server for Volt's static output.
 
 This module is just a simple wrapper for SimpleHTTPServer with more compact
-log message, and ability to look for Volt's site output directory provided
-the active directory is a subdirectory of Volt's root.
+log message and the option to set directory to serve. By default, it searches
+for the "site" directory and serves the contents.
 
 """
 
 import os
+import posixpath
 import sys
+import urllib
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
@@ -19,19 +21,8 @@ class VoltHTTPRequestHandler(SimpleHTTPRequestHandler):
     #server_version = 'VoltHTTP' + __version__
 
     def __init__(self, *args, **kwargs):
-        self.base_dir = self.find_base()
+        self.base_dir = os.path.join(os.getcwd(), 'site')
         SimpleHTTPRequestHandler.__init__(self, *args, **kwargs)
-
-    def find_base(self):
-        """Returns the base directory for static site output.
-
-        Made to enable server run from any subdirectory of Volt's root.
-
-        """
-        ### TODO-01
-        ### after proper directory tree has been decided
-        ### TEMP
-        return os.getcwd()
 
     def log_message(self, format, *args):
         """Prints the log message.
@@ -41,16 +32,34 @@ class VoltHTTPRequestHandler(SimpleHTTPRequestHandler):
         """
         sys.stderr.write("[%s] %s\n" % (self.log_date_time_string(), format%args)) 
 
-    ### TODO-01
-    ### after proper directory tree has been decided
-    ### TEMP
-#    def translate_path(self):
-#        """Returns filesystem path of from requests.
-#
-#        Overrides parent translate_path to enable output static site directory
-#        lookup.
-#
-#        """
+    def log_request(self, code='-', size='-'):
+        """Logs the accepted request.
+
+        Overrides parent log_request so 'size' can be set dynamically.
+
+        """
+        self.log_message('"%s" %s %s',
+                         self.requestline, str(code), str(size))
+
+    def translate_path(self, path):
+        """Returns filesystem path of from requests.
+
+        Overrides parent translate_path to enable custom directory setting.
+
+        """
+        path = path.split('?',1)[0]
+        path = path.split('#',1)[0]
+        path = posixpath.normpath(urllib.unquote(path))
+        words = path.split('/')
+        words = filter(None, words)
+        # the line below is the only overwrite
+        path = self.base_dir
+        for word in words:
+            drive, word = os.path.splitdrive(word)
+            head, word = os.path.split(word)
+            if word in (os.curdir, os.pardir): continue
+            path = os.path.join(path, word)
+        return path
 
 def main():
     
@@ -64,6 +73,7 @@ def main():
     sa = server.socket.getsockname()
 
     print "\nVolt development server running at %s:%s ..." % (sa[0], sa[1]) 
+    print "Serving contents of %s." % ("directory")
     print "CTRL-C to stop.\n"
 
     try:
