032c8f29d7117add46dcfbc6b4e98f60bd45d477
bow <bow@bow.web.id>
2012-01-20 08:03:33 +0100

    minor change to port error message

--- volt/server.py
+++ volt/server.py
@@ -115,7 +115,8 @@ def main():
             error_message = ERRORS[e.args[0]]
         except (AttributeError, KeyError):
             error_message = str(e)
-        sys.stderr.write("Error: %s\n\n" % error_message)
+        sys.stderr.write("Error: %s\n" % error_message)
+        sys.stderr.write("Exiting...\n\n")
         sys.exit(1)
 
     running_address, running_port = server.socket.getsockname()
