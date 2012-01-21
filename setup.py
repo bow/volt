66fee049844fef3bcfe62541f8cc8be7795ec4ba
bow <bow@bow.web.id>
2012-01-21 09:24:07 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+from volt import __version__
+
+
+version = __version__
+long_description = open("README.rst").read()
+
+setup(
+    name = "Volt",
+    version = version,
+    description = "Volt is a static website generator with extra features.",
+    long_description = long_description,
+    author = "Wibowo Arindrarto",
+    author_email = "bow@bow.web.id",
+    url = "",
+    license = "MIT",
+    packages = find_packages(),
+    install_requires = [
+        "pyyaml", 
+        "markdown", 
+        "jinja2"
+    ],
+    # test_suite,
+    # tests_require,
+    zip_safe = False,
+    entry_points = """
+    [console_scripts]
+    volt = volt.main:main
+    """,
+)
