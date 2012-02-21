4efb52f57df70932f27945b0e323c31456878432
bow <bow@bow.web.id>
2012-02-21 01:20:23 +0100

    updated: base engine and base item skeleton

--- volt/engine/__init__.py
+++ volt/engine/__init__.py
@@ -2,15 +2,54 @@ import os
 
 
 class BaseEngine(object):
-    # methods for:
-    # - parsing resource
-    # - constructing path for writing results
-    # - constructing urls
-    pass
+
+    def __init__(self, config):
+        """Initializes the engine
+
+        Arguments:
+        config: Engine Config object from volt.config.config
+        """
+        pass
+
+    def parse(self):
+        """Parses the content, returning BaseItem object.
+        """
+        # this should be a generator
+        raise NotImplementedError
+
+    def create_dirs(self):
+        """Creates all required directories in the site folder.
+        """
+        raise NotImplementedError
+
+    def build_paths(self):
+        """Builds all the required URLs.
+        """
+        raise NotImplementedError
+
+    def write_single(self):
+        """Writes a single BaseItem object to an output file.
+        """
+        raise NotImplementedError
+
+    def write_multiple(self):
+        """Writes an output file composed of multipe BaseItem object.
+        """
+        raise NotImplementedError
+
+    def start(self):
+        """Starts the engine processing.
+        """
+        raise NotImplementedError
 
 
-class BaseEngineGroup(object):
-    # for combining engines, so posts/pages
-    # are aware of each other's metadata
-    # lazy load? factories?
-    pass
+class BaseItem(object):
+
+    def __init__(self, header, content):
+        """Initializes a content item.
+
+        Arguments:
+        content: item data
+        header: item metadata
+        """
+        pass
