1ac780e5a636c7edd832c35db9af89a0cca2b703
bow <bow@bow.web.id>
2012-03-01 22:41:59 +0100

    updated: syntax plugin documentation

--- volt/plugin/syntax.py
+++ volt/plugin/syntax.py
@@ -1,6 +1,5 @@
 # Volt plugin for syntax highlight
 
-
 import os
 import re
 
@@ -16,31 +15,58 @@ _RE_SYNTAX = re.compile(r'(<syntax:(.*?)>(.*?)</syntax>)', re.DOTALL)
 
 
 class SyntaxHighlighter(Processor):
-    """Plugin for code highlighting using pygments.
+
+    """Highlights code syntax using pygments.
+
+    This processor plugin adds the necessary HTML tags to any text enclosed
+    in a ``<syntax:[language]></syntax>`` tag so that its syntaxes are highlighted.
+    It uses the pygments library to perform this feat, and can highlight any
+    language recognized by pygments, so long as it is specified by the
+    ``<syntax:[language]></syntax>`` tag.
+
+    To avoid unintended results, this plugin should be run before any markup
+    processing plugin.
+
+    Options for this plugin configurable via ``voltconf.py`` are:
+
+        `SYNTAX_CSS_CLASS`
+            String indicating the CSS class of the highlighted syntax, defaults
+            to ``'syntax'``.
+
+        `SYNTAX_CSS_FILE`
+            String indicating absolute path to the CSS file output, defaults to
+            ``syntax_highlight.css`` in the current directory.
+
+        `SYNTAX_LINENO`
+            Boolean indicating whether to output line numbers, defaults to
+            ``True``.
+
+        `SYNTAX_UNIT_FIELD`
+            String indicating which unit field to process, defaults to
+            ``'content'``.
     """
+
     DEFAULT_ARGS = {
             # class name for the highlighted syntax block
             'SYNTAX_CSS_CLASS': 'syntax',
-            # whether to output line numbers
-            'SYNTAX_LINENO': True,
             # css output for syntax highlight
             # defaults to current directory
-            'SYNTAX_CSS_FILE': os.path.join(os.getcwd(), 'syntax_highlight.css')
+            'SYNTAX_CSS_FILE': os.path.join(os.getcwd(), 'syntax_highlight.css'),
+            # whether to output line numbers
+            'SYNTAX_LINENO': True,
+            # unit field to process
+            'SYNTAX_UNIT_FIELD': 'content',
     }
 
     def process(self, units):
-        """Process the units
-
-        Arguments:
-        units: list containing units to process
-        """
+        """Process the given units."""
         for unit in units:
             # get content from unit
-            string = getattr(unit, 'content')
+            string = getattr(unit, CONFIG.PLUGINS.SYNTAX_UNIT_FIELD)
             # highlight syntax in content
             string = self.highlight_syntax(string)
             # override original content with syntax highlighted
-            setattr(unit, 'content', string)
+            setattr(unit, CONFIG.PLUGINS.SYNTAX_UNIT_FIELD, string)
 
         # write syntax highlight css file
         css = HtmlFormatter().get_style_defs('.' + CONFIG.PLUGINS.SYNTAX_CSS_CLASS)
@@ -49,12 +75,10 @@ class SyntaxHighlighter(Processor):
             target.write(css)
 
     def highlight_syntax(self, string):
-        """Highlights the syntaxes in the given string.
+        """Highlights syntaxes in the given string.
 
         Arguments:
-        string: string containing the code to highlight (marked by <syntax> tags)
-
-        Usually string is stored in unit.contents.
+        string: string containing the code to highlight.
         """
         codeblocks = re.findall(_RE_SYNTAX, string)
         # results: list of tuples of 3 items
@@ -68,6 +92,8 @@ class SyntaxHighlighter(Processor):
                 formatter = HtmlFormatter(linenos=CONFIG.PLUGINS.SYNTAX_LINENO, \
                         cssclass=CONFIG.PLUGINS.SYNTAX_CSS_CLASS)
                 highlighted = highlight(code, lexer, formatter)
+                # add 1 arg because replacement should only be done
+                # once for each match
                 string = string.replace(match, highlighted, 1)
 
         return string
