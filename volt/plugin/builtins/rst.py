65ffa0924da7009b14138a04b35ea514be743a01
bow <bow@bow.web.id>
2012-03-18 23:27:24 +0100

    added: initial rst plugin implementation

---
+++ volt/plugin/builtins/rst.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+------------------------
+volt.plugin.builtins.rst
+------------------------
+
+reStructuredText plugin for Volt units.
+
+:copyright: (c) 2012 Wibowo Arindrarto <bow@bow.web.id>
+:license: BSD
+
+"""
+
+from volt.plugin.core import Plugin
+
+
+class RstPlugin(Plugin):
+
+    """Plugin for transforming rST syntax to html."""
+
+    def run(self, units):
+        """Process the given units."""
+        pass
