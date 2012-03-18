ddd930a86a0ec148ce0931d2731f8386a72a2e0a
bow <bow@bow.web.id>
2012-03-19 00:51:49 +0100

    added: textile plugin initial implementation

---
+++ volt/plugin/builtins/textile_parser.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+-----------------------------------
+volt.plugin.builtins.textile_parser
+-----------------------------------
+
+Textile plugin for Volt units.
+
+:copyright: (c) 2012 Wibowo Arindrarto <bow@bow.web.id>
+:license: BSD
+
+"""
+
+import os
+
+import textile
+
+from volt.plugin.core import Plugin
+
+
+class TextileParserPlugin(Plugin):
+
+    """Plugin for transforming textile syntax to html."""
+
+    def run(self, units):
+        """Process the given units."""
+        for unit in units:
+            if hasattr(unit, 'markup'):
+                is_textile = ('textile' == getattr(unit, 'markup').lower())
+            else:
+                ext = os.path.splitext(unit.id)[1]
+                is_textile = (ext.lower() == '.textile')
+
+            if is_textile:
+                string = getattr(unit, 'content')
+                string = self.get_html(string)
+                setattr(unit, 'content', string)
+
+    def get_html(self, string):
+        return textile.textile(string)
