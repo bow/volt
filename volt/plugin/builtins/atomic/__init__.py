c0b06b444f37b2b760115c2fd5ce649de0c84d4e
bow <bow@bow.web.id>
2012-04-20 11:15:09 +0200

    fix: atomwrite encoding error for python 2

--- volt/plugin/builtins/atomic/__init__.py
+++ volt/plugin/builtins/atomic/__init__.py
@@ -13,6 +13,7 @@ Atom feed generator plugin.
 
 from __future__ import with_statement
 import os
+import sys
 from datetime import datetime
 
 from jinja2 import Environment, FileSystemLoader
@@ -75,4 +76,6 @@ class AtomicPlugin(Plugin):
         # render and write to output file
         rendered = template.render(units=engine.units[:10], CONFIG=CONFIG, time=time)
         with open(self.config.OUTPUT_FILE, 'w') as target:
+            if sys.version_info[0] < 3:
+                rendered = rendered.encode('utf-8')
             target.write(rendered)
