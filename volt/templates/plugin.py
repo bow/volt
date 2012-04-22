2a349d51cd62c64042075058e17a63a0a62493aa
bow <bow@bow.web.id>
2012-04-22 16:01:32 +0200

    updated: empty plugin template

--- volt/templates/plugin.py
+++ volt/templates/plugin.py
@@ -5,5 +5,12 @@ from volt.plugin.core import Plugin
 
 class MyPlugin(Plugin):
 
-    def run(self, units):
+    # Uncomment to set a default set of plugin configuration values
+    #DEFAULTS = Config(
+    #)
+
+    # Uncomment to set the plugin entry name in voltconf
+    #USER_CONF_ENTRY = ''
+
+    def run(self):
         pass
