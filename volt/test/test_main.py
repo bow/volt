b38efa758cd201454d3aefff13b8c599caffaa19
bow <bow@bow.web.id>
2012-05-16 01:09:51 +0200

    updated: travis config, test to handle __pycache__

--- volt/test/test_main.py
+++ volt/test/test_main.py
@@ -152,6 +152,11 @@ class MainInitCases(unittest.TestCase):
         before = [x for x in os.listdir(CMD_INIT_DIR) if not x.endswith('.pyc')]
         main.main(['gen'])
         after = [x for x in os.listdir(CMD_INIT_DIR) if not x.endswith('.pyc')]
+
+        # HACK: to handle __pycache__ directories
+        if '__pycache__' in after:
+            before.append('__pycache__')
+
         [x.sort() for x in before, after]
 
         self.assertEqual(before, after)
@@ -169,6 +174,11 @@ class MainInitCases(unittest.TestCase):
         before = [x for x in os.listdir(CMD_INIT_DIR) if not x.endswith('.pyc')]
         main.main(['serve'])
         after = [x for x in os.listdir(CMD_INIT_DIR) if not x.endswith('.pyc')]
+
+        # HACK: to handle __pycache__ directories
+        if '__pycache__' in after:
+            before.append('__pycache__')
+
         [x.sort() for x in before, after]
 
         self.assertEqual(before, after)
