6eb947dd623bd1f19ae21036e7c45630e96c44a9
Wibowo Arindrarto <contact@arindrarto.dev>
2022-11-03 08:49:14 +0100

    chore: Add hooks logger for users

--- volt/hooks.py
+++ volt/hooks.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2012-2022 Wibowo Arindrarto <contact@arindrarto.dev>
 # SPDX-License-Identifier: BSD-3-Clause
 
+import structlog
+
 from . import signals as s
 
 
@@ -10,6 +12,9 @@ __all__ = [
     "post_site_collect_targets",
     "pre_site_write",
 ]
+
+
+log = structlog.get_logger(__name__)
 
 
 post_site_load_engines = s.post_site_load_engines.connect
