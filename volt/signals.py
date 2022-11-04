2304dcf3ebf189d7c3b1a00211a288e359c4cbb5
Wibowo Arindrarto <contact@arindrarto.dev>
2022-11-04 17:50:44 +0100

    chore: Rename signals for consistency

--- volt/signals.py
+++ volt/signals.py
@@ -12,9 +12,9 @@ from structlog.contextvars import bound_contextvars
 log = structlog.get_logger(__name__)
 
 
-post_site_load_engines = signal("post-site-load-engines")
-post_site_collect_targets = signal("post-site-collect-targets")
-pre_site_write = signal("pre-site-write")
+post_site_load_engines = signal("post_site_load_engines")
+post_site_collect_targets = signal("post_site_collect_targets")
+pre_site_write = signal("pre_site_write")
 
 
 def send(signal: NamedSignal, *args: Any, **kwargs: Any) -> None:
