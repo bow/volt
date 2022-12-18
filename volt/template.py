48214aacc0da1a9708a967136d79209e755de749
Wibowo Arindrarto <contact@arindrarto.dev>
2022-12-18 05:53:55 +0100

    refactor: Add function to create template extension mark decorator

--- volt/template.py
+++ volt/template.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2012-2022 Wibowo Arindrarto <contact@arindrarto.dev>
 # SPDX-License-Identifier: BSD-3-Clause
 
-from typing import overload, Callable, Optional, ParamSpec, TypeVar
+from typing import Callable, Optional, ParamSpec, TypeVar
 
 from .constants import TEMPLATE_FILTER_MARK
 
@@ -14,25 +14,26 @@ T = TypeVar("T")
 P = ParamSpec("P")
 
 
-@overload
-def filter(__clb: Callable[P, T]) -> Callable[P, T]:
-    ...
+def _make_marker_decorator(
+    mark: str,
+) -> Callable[
+    [Optional[Callable[P, T]], Optional[str]],
+    Callable[P, T] | Callable[[Callable[P, T]], Callable[P, T]],
+]:
+    def func(
+        __clb: Optional[Callable[P, T]] = None,
+        name: Optional[str] = None,
+    ) -> Callable[P, T] | Callable[[Callable[P, T]], Callable[P, T]]:
+        def decorator(clb: Callable[P, T]) -> Callable[P, T]:
+            setattr(clb, TEMPLATE_FILTER_MARK, name or clb.__name__)
+            return clb
+
+        if __clb is None:
+            return decorator
+
+        return decorator(__clb)
+
+    return func
 
 
-@overload
-def filter(*, name: str) -> Callable[[Callable[P, T]], Callable[P, T]]:
-    ...
-
-
-def filter(
-    __clb: Optional[Callable[P, T]] = None,
-    name: Optional[str] = None,
-) -> Callable[P, T] | Callable[[Callable[P, T]], Callable[P, T]]:
-    def decorator(clb: Callable[P, T]) -> Callable[P, T]:
-        setattr(clb, TEMPLATE_FILTER_MARK, name or clb.__name__)
-        return clb
-
-    if __clb is None:
-        return decorator
-
-    return decorator(__clb)
+filter = _make_marker_decorator(TEMPLATE_FILTER_MARK)
