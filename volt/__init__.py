d0a3c25eb9b45c9f441c09079f9d455910cd32ee
Wibowo Arindrarto <contact@arindrarto.dev>
2024-07-20 23:19:40 +0200

    refactor: Replace single_source with importlib.metadata

--- volt/__init__.py
+++ volt/__init__.py
@@ -7,8 +7,8 @@
 """
 
 from pathlib import Path
+from importlib.metadata import version
 
-from single_source import get_version
 
 from .config import Config  # noqa: F401
 from .engines import Engine, MarkdownEngine  # noqa: F401
@@ -21,6 +21,6 @@ NAME = "volt"
 __author__ = "Wibowo Arindrarto"
 __contact__ = "contact@arindrarto.dev"
 __homepage__ = "https://github.com/bow/volt"
-__version__ = get_version(__name__, Path(__file__).parent.parent)
+__version__ = version(__name__)
 
-del Path, get_version
+del Path, version
